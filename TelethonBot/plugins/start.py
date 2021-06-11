6cbba9d107d70cbc38d03b3eef88ced0c9a0c2a4
dvstchannel <83810390+dvstchannel@users.noreply.github.com>
2021-06-12 06:24:18 +0700

    Update start.py

--- TelethonBot/plugins/start.py
+++ TelethonBot/plugins/start.py
@@ -13,8 +13,8 @@ async def start(event):
                     \n\n**• 👉 APP XEM IDOL LIVE:** http://00ff.live ",
                     buttons=[
                         [
-                            Button.url("FACEBOOK🌚",url="https://www.facebook.com/groups/2754080448238156/?multi_permalinks=2789031428076391&notif_id=1623451636366372&notif_t=feedback_reaction_generic&ref=notif"),
-                            Button.url("twitter🌝",url="https://twitter.com/botocsex")
+                            Button.url("FACEBOOK 🌚",url="https://www.facebook.com/groups/2754080448238156/?multi_permalinks=2789031428076391&notif_id=1623451636366372&notif_t=feedback_reaction_generic&ref=notif"),
+                            Button.url("TWITTER 🌝",url="https://twitter.com/botocsex")
                         ],
                         [
                             Button.url("APP LIVE FREE LUÔN",url="https://t.me/joinchat/HFMeDVI4K_czMjBl")
