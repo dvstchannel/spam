95e297497c29081d4bb40437c6fd5d3b82d3f353
dvstchannel <83810390+dvstchannel@users.noreply.github.com>
2021-06-13 13:21:13 +0700

    Update start.py

--- TelethonBot/plugins/start.py
+++ TelethonBot/plugins/start.py
@@ -8,16 +8,16 @@ from telethon import events, Button
 @BotzHub.on(events.NewMessage(incoming=True))
 async def start(event):
     await event.reply("🧚‍♀️🧚‍♀️🧚‍♀️🧚‍♀️🧚‍♀️🧚‍♀️🧚‍♀️🧚‍♀️🧚‍\n.\
-                    \n**• LINK MỚI ANH EM ƠI!!! CHỊ ÁO ĐỎ, CÓ HÌNH CON THỎ**\
-                    \n\n**• 👉 Nhóm Live ZALO FREE [𝗫𝗘𝗠 𝗡𝗚𝗔𝗬](https://zalo.me/g/mjpxri305) **\
+                    \n**• LINK Mọi người có biết tin của chị bên đội malay chưa ???**\
+                    \n\n**• 👉 ZALO CHAT [𝗫𝗘𝗠 𝗡𝗚𝗔𝗬](https://zalo.me/g/mjpxri305) **\
                     \n\n**• 👉 APP XEM IDOL LIVE:** http://00ff.live ",
                     buttons=[
                         [
-                            Button.url("FACEBOOK 🌚",url="https://www.facebook.com/groups/2754080448238156/?multi_permalinks=2789031428076391&notif_id=1623451636366372&notif_t=feedback_reaction_generic&ref=notif"),
+                            Button.url("HÓNG NÈ 🌚",url="https://t.me/joinchat/nfBmvkS4FeFjYjE1"),
                             Button.url("TWITTER 🌝",url="https://twitter.com/botocsex")
                         ],
                         [
-                            Button.url("APP LIVE FREE LUÔN",url="https://t.me/joinchat/HFMeDVI4K_czMjBl")
+                            Button.url("Hóng Hớt TV",url="https://t.me/joinchat/HFMeDVI4K_czMjBl")
                         ]
                     ])
     
