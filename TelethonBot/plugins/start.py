d5352862b54b276ab2924a14d4dd33d625a97112
dvstchannel <83810390+dvstchannel@users.noreply.github.com>
2021-06-13 15:44:38 +0700

    Update start.py

--- TelethonBot/plugins/start.py
+++ TelethonBot/plugins/start.py
@@ -8,16 +8,16 @@ from telethon import events, Button
 @BotzHub.on(events.NewMessage(incoming=True))
 async def start(event):
     await event.reply("🧚‍♀️🧚‍♀️🧚‍♀️🧚‍♀️🧚‍♀️🧚‍♀️🧚‍♀️🧚‍♀️🧚‍\n.\
-                    \n**•  ???**\
+                    \n**• ROOM IDOL NHIỀU NHẤT TELEGRAM TÔI HỨA LUÔN ???**\
                     \n\n**• 👉 ZALO CHAT [𝗫𝗘𝗠 𝗡𝗚𝗔𝗬](https://zalo.me/g/hamlsi428) **\
                     \n\n**• 👉 APP XEM IDOL LIVE:** QQL849.com ",
                     buttons=[
                         [
-                            Button.url("GÁI SG  🌚",url="https://t.me/joinchat/nfBmvkS4FeFjYjE1"),
+                            Button.url("ĐỘNG GÁI🌚",url="https://t.me/joinchat/nfBmvkS4FeFjYjE1"),
                             Button.url("TWITTER 🌝",url="https://twitter.com/botocsex")
                         ],
                         [
-                            Button.url("Hóng Hớt TV",url="https://t.me/joinchat/HFMeDVI4K_czMjBl")
+                            Button.url("ĐỪNG CÓ TÒ MÒ OKE!!!",url="https://t.me/joinchat/HFMeDVI4K_czMjBl")
                         ]
                     ])
     
